9551327ef71ea30fe06bb28b306e711b986175b6
Ari Moniz <amoniz@csumb.edu>
2022-02-14 10:49:58 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+//How to make your cursor indicate that something can be grabbed
+//Design site how you please
+//Create a div within the body
+//div {
+//    cursor:grab;
+//}
+
+// Define Variable
+  var make - "Hyundai";
+  var model = "Sonota";
+  var color = "White";
+  var year = 2013;
+
+  //math
+  age - 2022 - year;
+
+  //Output
+    document.writeln("Make: " + make + "<br>");
+    document.writeln("Model: " + model + "<br>");
+    document.writeln("Color: " + color + "<br>");
+    document.writeln("Year: " + year + "<br>");
+    document.writeln("Age: " + age + "<br>");
