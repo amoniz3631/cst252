5d74844a396870358490412c14841254b73507c4
Ari Moniz <amoniz@csumb.edu>
2022-02-14 19:34:28 -0800

    dfhdsj

---
+++ lab6/js/lab.js
@@ -0,0 +1,16 @@
+//define variables
+myTransport = ["Hyundai Sonota", "walking", "carpool"];
+
+var myMainRide = {
+  make = "Hyundai"
+  model = "Sonota"
+  color = "White"
+  year = 2013
+  age = function() {
+    return 2019 - age;
+    }
+}
+//Output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+document.writeln("My main ride: <pre>");
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
