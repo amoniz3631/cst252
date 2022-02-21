f64a1b101a0b7aa582e938f7f9e2d80200efb350
Ari Moniz <amoniz@csumb.edu>
2022-02-21 10:21:51 -0800

    ,m

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -2,15 +2,14 @@
 myTransport = ["Hyundai Sonota", "walking", "carpool"];
 
 var myMainRide = {
-  make = "Hyundai"
-  model = "Sonota"
-  color = "White"
-  year = 2013
+  make: "Hyundai",
+  model: "Sonota",
+  color: "White",
+  year: 2013,
   age = function() {
     return 2019 - age;
     }
 }
 //Output
 document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
-document.writeln("My main ride: <pre>");
-  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+document.writeln("My main ride: <pre>" + JSON.stringify(myMainRide, null, '\t') + "</pre>");
