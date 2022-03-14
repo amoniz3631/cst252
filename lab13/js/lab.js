9122bfc987f1b08fc24cacdf9f1b3853362206f9
Ari Moniz <amoniz@csumb.edu>
2022-03-14 12:11:04 -0700

    t

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,36 +1,27 @@
-/**
- * Author: Sara Christensen and Ari Moniz
- * Created:   2.21.22
- **/
-
-      function sortUserName() {
-        // declare a variable userName and use window.prompt()
-            var userName = window.prompt("Enter your name");
-            console.log("userName = ", userName);
-        // Sort the letters of the user's name and return those from the function
-        // split string into array
-            var nameArray = userName.split('');
-            console.log("nameArray = ", nameArray);
-        // sort array
-            var nameArraySort = nameArray.sort();
-            console.log("nameArraySort = ", nameArraySort);
-        // convert array back to string
-            var nameSorted = nameArraySort.join('');
-            console.log("nameSorted = ", nameSorted);
-        // return sorted string
-            return nameSorted;
+function sortingHat(name) {
+  // get length of name
+  var len = name.length;
+  // do a mod 4 to get house
+  var mod = len % 4;
+  //if conditional to set houseStr to houseStr
+  if (mod == 0) {
+    houseStr = "Ravenclaw";
+  } else if (mod == 1){
+    houseStr = "Slytherin";
+  } else if (mod == 2){
+    houseStr = "Hufflepuff";
+  } else if (mod == 3){
+    houseStr = "Gryffindor";
   }
-
-        // sorting capital letters together
-        // https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
-function sortCapLetters(str) {
-  return str.replace(
-    /\w\S*/g,
-    function(txt) {
-      return txt.charAt(0).sortCapLetters() + txt.substr(1).toLowerCase();
-    }
-  );
+  //return house txt
+  return houseStr;
 }
 
-      // call the function and output the results with document.write()
-    document.writeln("Your new name is: ", sortUserName());
+$("#my-button").click(function(){
+  // need id input in html
+  var name = $("#input").val();
+  var house = sortingHat(name);
+  console.log(house);
+  // need id output in html
+  $("#output").html(house);
+})
