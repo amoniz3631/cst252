7ba36026ff6dde756a8e88cf117032c6dafbcf4d
Ari Moniz <amoniz@csumb.edu>
2022-04-03 17:38:56 -0700

    Lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,36 +1,17 @@
 /**
- * Author: Sara Christensen and Ari Moniz
- * Created:   2.21.22
+ * Author:    Sara Christensen and Ari Moniz
+ * Created:   3.9.2022
  **/
 
-      function sortUserName() {
-        // declare a variable userName and use window.prompt()
-            var userName = window.prompt("Enter your name");
-            console.log("userName = ", userName);
-        // Sort the letters of the user's name and return those from the function
-        // split string into array
-            var nameArray = userName.split('');
-            console.log("nameArray = ", nameArray);
-        // sort array
-            var nameArraySort = nameArray.sort();
-            console.log("nameArraySort = ", nameArraySort);
-        // convert array back to string
-            var nameSorted = nameArraySort.join('');
-            console.log("nameSorted = ", nameSorted);
-        // return sorted string
-            return nameSorted;
-  }
+ $("#subject-button").click(function(){
+   $("#subject").toggleClass("green")
+ })
 
-        // sorting capital letters together
-        // https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
-function sortCapLetters(str) {
-  return str.replace(
-    /\w\S*/g,
-    function(txt) {
-      return txt.charAt(0).sortCapLetters() + txt.substr(1).toLowerCase();
-    }
-  );
-}
+ $("#difficulty-button").click(function(){
+   $("#difficulty").toggleClass("orange")
+ })
 
-      // call the function and output the results with document.write()
-    document.writeln("Your new name is: ", sortUserName());
+ $("#result-button").click(function(){
+   $("#result").toggleClass("brown")
+   $("#result").toggleClass("big")
+ })
