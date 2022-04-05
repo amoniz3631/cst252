d2457596d6be4a6d70d9cc7d9081460f17966b1f
Ari Moniz <amoniz@csumb.edu>
2022-04-05 12:43:25 -0700

    lab14

---
+++ lab15/js/lab.js
@@ -0,0 +1,29 @@
+function sortingHat(name) {
+  // get length of name
+  var len = name.length;
+  // do a mod 4 to get house
+  var mod = len % 4;
+  //if conditional to set houseStr to houseStr
+  if (mod == 0) {
+    houseStr = "Ravenclaw";
+  } else if (mod == 1){
+    houseStr = "Slytherin";
+  } else if (mod == 2){
+    houseStr = "Hufflepuff";
+  } else if (mod == 3){
+    houseStr = "Gryffindor";
+  }
+  //return house txt
+  return houseStr;
+}
+
+$("#button").click(function(){
+  // need id input in html
+  var name = $("#input").val();
+  var house = sortingHat(name);
+  console.log(house);
+  // need id output in html
+  $("#output").html(house);
+  var newText = "The Sorting Hat has sorted you into " + house;
+    document.getElementById("output").innerHTML = newText;
+})
